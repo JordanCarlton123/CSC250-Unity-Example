c463325cf50058cc04c96592f59ac62d61dec740
JordanCarlton123 <jordan.carlton@cuw.edu>
2022-04-12 21:27:59 -0500

    Added rigid bodies between player and enemy

--- My project (1)/Assets/scripts/CORE.cs
+++ My project (1)/Assets/scripts/CORE.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class COREScript : MonoBehaviour
+public class CORE : MonoBehaviour
 {
-    public Transform enemy;
+    public Transform enemyPrefab;
     public Transform spawnPoint;
+
+    public static List<GameObject> theRooms = new List<GameObject>();
+
+    public static void addRoomGO(GameObject go)
+    {
+        CORE.theRooms.Add(go);
+        print("Added Room");
+    }
 
     public static void display()
     {
@@ -15,12 +23,18 @@ public class COREScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemy, spawnPoint.position, Quaternion.idnentity);
+        for (int i = 0; i < 100; i++)
+        {
+            Transform t = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+            Rigidbody rb = t.GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(Random.Range(10, 30), 0, Random.Range(10, 30));
+        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- My project (1)/Assets/scripts/boxScript.cs
+++ My project (1)/Assets/scripts/boxScript.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class boxScript : MonoBehaviour
 {
     private int count;
+    public GameObject thePlayer;
+    private Vector3 playerPosition;
+    private Rigidbody rb;
+    public float speed = 20f;
+    private UnityEngine.AI.NavMeshAgent agent;
+    //agent.Warp(thePlayer.transform.position);
 
     // Start is called before the first frame update
     void Start() //like a constructor
     {
         count = 0;
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        agent = this.gameObject.GetComponent<NavMeshAgent>();
+        //agent.speed = 20f;
+        //agent.Warp(thePlayer.transform.position); 
+
     }
 
     void OnCollisionEnter(Collision collision)
@@ -29,6 +41,6 @@ public class boxScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        agent.SetDestination(thePlayer.transform.position);
     }
 }
